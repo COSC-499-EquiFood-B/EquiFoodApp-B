2c5a59d178dafe1c2ec2fbca956bd2fba36f60af
Anamica S <71396652+anamicasunder@users.noreply.github.com>
2022-10-28 10:05:58 -0700

    Update indiv_dashboard_widget.dart

--- lib/indiv_dashboard/indiv_dashboard_widget.dart
+++ lib/indiv_dashboard/indiv_dashboard_widget.dart
@@ -1,220 +1,100 @@
-import '../flutter_flow/flutter_flow_icon_button.dart';
-import '../flutter_flow/flutter_flow_place_picker.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
-import '../flutter_flow/flutter_flow_widgets.dart';
-import '../flutter_flow/place.dart';
-import 'dart:io';
+import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomePageWidget extends StatefulWidget {
-  const HomePageWidget({Key? key}) : super(key: key);
+class HomepageWidget extends StatefulWidget {
+  const HomepageWidget({Key? key}) : super(key: key);
 
   @override
-  _HomePageWidgetState createState() => _HomePageWidgetState();
+  _HomepageWidgetState createState() => _HomepageWidgetState();
 }
-//home page widget state
-class _HomePageWidgetState extends State<HomePageWidget> {
-  double? ratingBarValue1;
-  var placePickerValue = FFPlace();
-  double? ratingBarValue2;
+
+class _HomepageWidgetState extends State<HomepageWidget> {
+  TextEditingController? textController;
   final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    textController?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
       backgroundColor: Color(0xFFACE4AF),
-      body: SafeArea(
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
-          child: Stack(
-            children: [
-              Align(
-                alignment: AlignmentDirectional(0, 1.2),
-                child: Container(
-                  width: 420,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: FlutterFlowTheme.of(context).secondaryBackground,
-                  ),
-                ),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFACE4AF),
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Hello, (insert account name)',
+          style: FlutterFlowTheme.of(context).title2.override(
+                fontFamily: 'Outfit',
+                color: Color(0xFF14181B),
+                fontSize: 25,
+                fontWeight: FontWeight.normal,
               ),
-              Stack(
-                children: [
-                  Stack(
-                    children: [
-                      Stack(
-                        children: [
-                          Align(
-                            alignment: AlignmentDirectional(-0.9, -1),
-                            child: FlutterFlowIconButton(
-                              borderColor: Colors.transparent,
-                              borderRadius: 30,
-                              borderWidth: 1,
-                              buttonSize: 50,
-                              icon: Icon(
-                                Icons.arrow_back_sharp,
-                                color:
-                                    FlutterFlowTheme.of(context).primaryBtnText,
-                                size: 25,
-                              ),
-                              onPressed: () {
-                                print('IconButton pressed ...');
-                              },
-                            ),
-                          ),
-                          Align(
-                            alignment: AlignmentDirectional(-0.24, -0.99),
-                            child: FlutterFlowPlacePicker(
-                              iOSGoogleMapsApiKey: '',
-                              androidGoogleMapsApiKey: '',
-                              webGoogleMapsApiKey: '',
-                              onSelect: (place) async {
-                                setState(() => placePickerValue = place);
-                              },
-                              defaultText: 'Select Location',
-                              icon: Icon(
-                                Icons.place,
-                                color: Colors.white,
-                                size: 16,
-                              ),
-                              buttonOptions: FFButtonOptions(
-                                width: 200,
-                                height: 40,
-                                color: Color(0xFFACE4AF),
-                                textStyle: FlutterFlowTheme.of(context)
-                                    .subtitle2
-                                    .override(
-                                      fontFamily: 'Poppins',
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                    ),
-                                borderSide: BorderSide(
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(12),
-                              ),
-                            ),
-                          ),
-                          Align(
-                            alignment: AlignmentDirectional(0.52, -1.02),
-                            child: FlutterFlowIconButton(
-                              borderColor: Colors.transparent,
-                              borderRadius: 30,
-                              borderWidth: 1,
-                              buttonSize: 60,
-                              icon: Icon(
-                                Icons.arrow_drop_down_rounded,
-                                color:
-                                    FlutterFlowTheme.of(context).primaryBtnText,
-                                size: 30,
-                              ),
-                              onPressed: () {
-                                print('IconButton pressed ...');
-                              },
-                            ),
-                          ),
-                          Align(
-                            alignment: AlignmentDirectional(-0.19, -0.85),
-                            child: Text(
-                              'Find available food \nnear you',
-                              textAlign: TextAlign.justify,
-                              style: FlutterFlowTheme.of(context)
-                                  .bodyText1
-                                  .override(
-                                    fontFamily: 'Poppins',
-                                    color: Colors.black,
-                                    fontSize: 30,
-                                  ),
-                            ),
-                          ),
-                          Align(
-                            alignment: AlignmentDirectional(-0.06, -0.58),
-                            child: FFButtonWidget(
-                              onPressed: () {
-                                print('Button pressed ...');
-                              },
-                              text: 'Search Restaurants',
-                              icon: Icon(
-                                Icons.search,
-                                color: Color(0xFFCACACA),
-                                size: 10,
-                              ),
-                              options: FFButtonOptions(
-                                width: 320,
-                                height: 40,
-                                color: Color(0x99FFFFFF),
-                                textStyle: FlutterFlowTheme.of(context)
-                                    .bodyText1
-                                    .override(
-                                      fontFamily: 'Poppins',
-                                      color: Color(0xFFCACACA),
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                borderSide: BorderSide(
-                                  color: Colors.transparent,
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                          Align(
-                            alignment: AlignmentDirectional(-0.84, -0.45),
-                            child: FFButtonWidget(
-                              onPressed: () {
-                                print('Button pressed ...');
-                              },
-                              text: 'Browse',
-                              options: FFButtonOptions(
-                                height: 40,
-                                padding:
-                                    EdgeInsetsDirectional.fromSTEB(3, 0, 0, 0),
-                                color: Color(0x00FFFFFF),
-                                textStyle: FlutterFlowTheme.of(context)
-                                    .subtitle2
-                                    .override(
-                                      fontFamily: 'Poppins',
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                borderSide: BorderSide(
-                                  color: Colors.transparent,
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                          Align(
-                            alignment: AlignmentDirectional(-0.84, -0.45),
-                            child: FFButtonWidget(
-                              onPressed: () {
-                                print('Button pressed ...');
-                              },
-                              text: 'Browse',
-                              options: FFButtonOptions(
-                                height: 40,
-                                padding:
-                                    EdgeInsetsDirectional.fromSTEB(3, 0, 0, 0),
-                                color: Color(0x00FFFFFF),
-                                textStyle: FlutterFlowTheme.of(context)
-                                    .subtitle2
-                                    .override(
-                                      fontFamily: 'Poppins',
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.normal,
-                                    ),
-                                borderSide: BorderSide(
-                                  color: Colors.transparent,
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                          ),
-                          Align(
-
+        ),
+        actions: [],
+        centerTitle: false,
+        elevation: 0,
+      ),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
+                child: TextFormField(
+                  controller: textController,
+                  onChanged: (_) => EasyDebounce.debounce(
+                    'textController',
+                    Duration(milliseconds: 2000),
+                    () => setState(() {}),
+                  ),
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    labelText: 'Search restaurants...',
+                    labelStyle: FlutterFlowTheme.of(context).bodyText2.override(
+                          fontFamily: 'Outfit',
+                          color: Color(0xFF57636C),
+                          fontSize: 12,
+                          fontWeight: FontWeight.normal,
+                        ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0x00000000),
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0x00000000),
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0x00000000),
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0x00000000),
+                        width: 1,
