e1c9785322a42c72606803ba01fc63266b5d6590
Neilansh Rajpal <60074628+NeilanshTriesToCode@users.noreply.github.com>
2022-10-27 21:49:04 -0700

    Created settings.dart in restaurant_dashboard folder
    - Created settings.dart.
    - Page for Settings used by Restaurants.
    - Draft UI.

---
+++ lib/restaurant_dashboard/settings.dart
@@ -0,0 +1,161 @@
+import '../flutter_flow/flutter_flow_icon_button.dart';
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import '../flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SettingsWidget extends StatefulWidget {
+  const SettingsWidget({Key? key}) : super(key: key);
+
+  @override
+  _SettingsWidgetState createState() => _SettingsWidgetState();
+}
+
+class _SettingsWidgetState extends State<SettingsWidget> {
+  bool? switchListTileValue1;
+  bool? switchListTileValue2;
+  bool? switchListTileValue3;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
+      appBar: AppBar(
+        backgroundColor: FlutterFlowTheme.of(context).secondaryBackground,
+        automaticallyImplyLeading: false,
+        leading: FlutterFlowIconButton(
+          borderColor: Colors.transparent,
+          borderRadius: 30,
+          borderWidth: 1,
+          buttonSize: 60,
+          icon: Icon(
+            Icons.arrow_back_rounded,
+            color: FlutterFlowTheme.of(context).primaryText,
+            size: 30,
+          ),
+          onPressed: () {
+            print('IconButton pressed ...');
+          },
+        ),
+        title: Text(
+          'Settings',
+          style: FlutterFlowTheme.of(context).title2.override(
+                fontFamily: 'Poppins',
+                color: FlutterFlowTheme.of(context).primaryText,
+                fontSize: 22,
+              ),
+        ),
+        actions: [],
+        centerTitle: false,
+        elevation: 0,
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Text(
+                    'Choose what notifcations you want to recieve below and we will update the settings.',
+                    style: FlutterFlowTheme.of(context).bodyText2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+            child: SwitchListTile.adaptive(
+              value: switchListTileValue1 ??= true,
+              onChanged: (newValue) async {
+                setState(() => switchListTileValue1 = newValue!);
+              },
+              title: Text(
+                'Push Notifications',
+                style: FlutterFlowTheme.of(context).title3,
+              ),
+              subtitle: Text(
+                'Receive Push notifications from our application on a semi regular basis.',
+                style: FlutterFlowTheme.of(context).bodyText2,
+              ),
+              activeColor: Color(0xFF4BA219),
+              activeTrackColor: Color(0x8A4B39EF),
+              dense: false,
+              controlAffinity: ListTileControlAffinity.trailing,
+              contentPadding: EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+            ),
+          ),
+          SwitchListTile.adaptive(
+            value: switchListTileValue2 ??= true,
+            onChanged: (newValue) async {
+              setState(() => switchListTileValue2 = newValue!);
+            },
+            title: Text(
+              'Email Notifications',
+              style: FlutterFlowTheme.of(context).title3,
+            ),
+            subtitle: Text(
+              'Receive email notifications from our marketing team about new features.',
+              style: FlutterFlowTheme.of(context).bodyText2,
+            ),
+            activeColor: Color(0xFF4BA219),
+            activeTrackColor: Color(0xFF3B2DB6),
+            dense: false,
+            controlAffinity: ListTileControlAffinity.trailing,
+            contentPadding: EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+          ),
+          SwitchListTile.adaptive(
+            value: switchListTileValue3 ??= true,
+            onChanged: (newValue) async {
+              setState(() => switchListTileValue3 = newValue!);
+            },
+            title: Text(
+              'Location Services',
+              style: FlutterFlowTheme.of(context).title3,
+            ),
+            subtitle: Text(
+              'Allow us to track your location, this helps keep track of spending and keeps you safe.',
+              style: FlutterFlowTheme.of(context).bodyText2,
+            ),
+            activeColor: Color(0xFF4BA219),
+            activeTrackColor: Color(0xFF3B2DB6),
+            dense: false,
+            controlAffinity: ListTileControlAffinity.trailing,
+            contentPadding: EdgeInsetsDirectional.fromSTEB(24, 12, 24, 12),
+          ),
+          Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+            child: FFButtonWidget(
+              onPressed: () async {
+                context.pop();
+              },
+              text: 'Change Changes',
+              options: FFButtonOptions(
+                width: 190,
+                height: 50,
+                color: Color(0xFF4BA219),
+                textStyle: FlutterFlowTheme.of(context).subtitle2.override(
+                      fontFamily: 'Lexend Deca',
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                elevation: 3,
+                borderSide: BorderSide(
+                  color: Colors.transparent,
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
