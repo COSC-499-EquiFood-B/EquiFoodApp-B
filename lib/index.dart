a9d6bf261eabff4427185269cc2776896e07243d
minhhbt <buithienminh@gmail.com>
2022-11-12 17:43:00 -0800

    Update iindex.dart

--- lib/index.dart
+++ lib/index.dart
@@ -1,8 +1,3 @@
 // Export pages
-export 'pickup_map/pickup_map_widget.dart' show PickupMapWidget;
-export 'register/register_widget.dart' show RegisterWidget;
-export 'login/login_widget.dart' show LoginWidget;
-export 'restaurant_dashboard/restaurant_dashboard_widget.dart'
-    show RestaurantDashboardWidget;
-export 'indiv_dashboard/indiv_dashboard_widget.dart' show IndivDashboardWidget;
 export 'indiv_item/indiv_item_widget.dart' show IndivItemWidget;
+export 'pickup_map/pickup_map_widget.dart' show PickupMapWidget;
