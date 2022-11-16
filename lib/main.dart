fa70adc456dc58d55a8e4aa9a5333032b3a957b2
minhhbt <buithienminh@gmail.com>
2022-11-15 22:06:52 -0800

    Fix screen not showing errors due to not provided Firebase info

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,15 @@ import 'index.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBM8lbMjhIu0Y6fCGhxSbM__dQ09H4UGGU",
+      appId: "1:490906039491:android:7fb1eb11a7a93d8e329e76",
+      messagingSenderId: "490906039491",
+      projectId: "equifood-teamb",
+    ),
+  );
   await FlutterFlowTheme.initialize();
 
   runApp(MyApp());
