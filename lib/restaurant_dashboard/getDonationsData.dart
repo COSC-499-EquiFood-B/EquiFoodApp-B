71f231852ba2a6b6773b1328ca45f7e542f84b38
NeilanshTriesToCode <neilanshjr@gmail.com>
2022-11-25 18:45:17 -0800

    created a new file containing widget to read from donations collection and populate the Card on Dashboard

---
+++ lib/restaurant_dashboard/getDonationsData.dart
@@ -0,0 +1,142 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../flutter_flow/flutter_flow_icon_button.dart';
+import '../flutter_flow/flutter_flow_theme.dart';
+
+class GetDonationsData extends StatelessWidget {
+  final String donationsID;
+
+  GetDonationsData({required this.donationsID});
+
+  @override
+  Widget build(BuildContext context) {
+    // create reference to the "donations" Collection in firebase
+    CollectionReference donations =
+        FirebaseFirestore.instance.collection('donations');
+
+    return FutureBuilder<DocumentSnapshot>(
+        future: donations.doc(donationsID).get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> donationsData =
+                snapshot.data!.data() as Map<String, dynamic>;
+
+            return Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(16, 16, 16, 16),
+              child: Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.network(
+                      'https://hot-thai-kitchen.com/wp-content/uploads/2021/10/Untitled-design-5.jpg',
+                      width: double.infinity,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+                      child: Container(
+                        width: double.infinity,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              '${donationsData["item_name"]} (${donationsData["quantity"]} servings)',
+                              style: FlutterFlowTheme.of(context).subtitle1,
+                            ),
+                            Container(
+                              width: 100,
+                              height: 100,
+                              decoration: BoxDecoration(
+                                color: FlutterFlowTheme.of(context)
+                                    .secondaryBackground,
+                              ),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  FlutterFlowIconButton(
+                                    borderColor: Colors.transparent,
+                                    borderRadius: 10,
+                                    borderWidth: 1,
+                                    buttonSize: 60,
+                                    icon: Icon(
+                                      Icons.edit_sharp,
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryText,
+                                      size: 30,
+                                    ),
+                                    onPressed: () {
+                                      print('IconButton pressed ...');
+                                    },
+                                  ),
+                                  FlutterFlowIconButton(
+                                    borderColor: Colors.transparent,
+                                    borderRadius: 20,
+                                    borderWidth: 1,
+                                    buttonSize: 60,
+                                    icon: Icon(
+                                      Icons.delete,
+                                      color: FlutterFlowTheme.of(context)
+                                          .primaryText,
+                                      size: 30,
+                                    ),
+                                    onPressed: () async {
+                                      var confirmDialogResponse =
+                                          await showDialog<bool>(
+                                                context: context,
+                                                builder: (alertDialogContext) {
+                                                  return AlertDialog(
+                                                    content: Text(
+                                                        'Remove Donation?'),
+                                                    actions: [
+                                                      TextButton(
+                                                        onPressed: () =>
+                                                            Navigator.pop(
+                                                                alertDialogContext,
+                                                                false),
+                                                        child: Text('Cancel'),
+                                                      ),
+                                                      TextButton(
+                                                        onPressed: () =>
+                                                            Navigator.pop(
+                                                                alertDialogContext,
+                                                                true),
+                                                        child: Text('Confirm'),
+                                                      ),
+                                                    ],
+                                                  );
+                                                },
+                                              ) ??
+                                              false;
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+          return Text('Loading...');
+        });
+  }
+}
--- lib/restaurant_dashboard/restaurant_dashboard_widget_2.dart
+++ lib/restaurant_dashboard/restaurant_dashboard_widget_2.dart
@@ -8,6 +8,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// import Widget to display Donations Data for the Restaurant User
+import './getDonationsData.dart';
+
 class DonationsWidget extends StatefulWidget {
   const DonationsWidget({Key? key}) : super(key: key);
 
@@ -40,6 +43,31 @@ class _DonationsWidgetState extends State<DonationsWidget> {
               })
             });
   }
+
+  int test() {
+    return 5;
+  }
+
+  // Future<Map<String, dynamic>> getDonationsData(String donationsID) async {
+  //   // create reference to "donations" Collection in firebase
+  //   CollectionReference donations =
+  //       FirebaseFirestore.instance.collection('donations');
+
+  //   Map<String, dynamic> donationsData = {};
+
+  //   await FirebaseFirestore.instance
+  //       .collection('donations')
+  //       .doc(donationsID)
+  //       .get()
+  //       .then((snapshot) => {
+  //             donationsData = {
+  //               'name': snapshot.data()!['name'],
+  //               'quantity': snapshot.data()!['quantity']
+  //             }
+  //           });
+
+  //   return donationsData;
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -114,139 +142,8 @@ class _DonationsWidgetState extends State<DonationsWidget> {
                             shrinkWrap: true, // required for infinite scrolling
                             itemCount: restaurantDonationIDs.length,
                             itemBuilder: (context, int index) {
-                              return /*ListTile(
-                                title: Text(restaurantDonationIDs[index]),
-                              ); */
-                                  Padding(
-                                padding: EdgeInsetsDirectional.fromSTEB(
-                                    16, 16, 16, 16),
-                                child: Card(
-                                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                                  elevation: 10,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15),
-                                  ),
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Image.network(
-                                        'https://hot-thai-kitchen.com/wp-content/uploads/2021/10/Untitled-design-5.jpg',
-                                        width: double.infinity,
-                                        height: 100,
-                                        fit: BoxFit.cover,
-                                      ),
-                                      Padding(
-                                        padding: EdgeInsetsDirectional.fromSTEB(
-                                            8, 8, 8, 8),
-                                        child: Container(
-                                          width: double.infinity,
-                                          height: 50,
-                                          decoration: BoxDecoration(
-                                            color: FlutterFlowTheme.of(context)
-                                                .secondaryBackground,
-                                          ),
-                                          child: Row(
-                                            mainAxisSize: MainAxisSize.min,
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Text(
-                                                'Pad Thai',
-                                                style:
-                                                    FlutterFlowTheme.of(context)
-                                                        .subtitle1,
-                                              ),
-                                              Container(
-                                                width: 100,
-                                                height: 100,
-                                                decoration: BoxDecoration(
-                                                  color: FlutterFlowTheme.of(
-                                                          context)
-                                                      .secondaryBackground,
-                                                ),
-                                                child: Row(
-                                                  mainAxisSize:
-                                                      MainAxisSize.max,
-                                                  mainAxisAlignment:
-                                                      MainAxisAlignment.end,
-                                                  children: [
-                                                    FlutterFlowIconButton(
-                                                      borderColor:
-                                                          Colors.transparent,
-                                                      borderRadius: 10,
-                                                      borderWidth: 1,
-                                                      buttonSize: 60,
-                                                      icon: Icon(
-                                                        Icons.edit_sharp,
-                                                        color:
-                                                            FlutterFlowTheme.of(
-                                                                    context)
-                                                                .primaryText,
-                                                        size: 30,
-                                                      ),
-                                                      onPressed: () {
-                                                        print(
-                                                            'IconButton pressed ...');
-                                                      },
-                                                    ),
-                                                    FlutterFlowIconButton(
-                                                      borderColor:
-                                                          Colors.transparent,
-                                                      borderRadius: 20,
-                                                      borderWidth: 1,
-                                                      buttonSize: 60,
-                                                      icon: Icon(
-                                                        Icons.delete,
-                                                        color:
-                                                            FlutterFlowTheme.of(
-                                                                    context)
-                                                                .primaryText,
-                                                        size: 30,
-                                                      ),
-                                                      onPressed: () async {
-                                                        var confirmDialogResponse =
-                                                            await showDialog<
-                                                                    bool>(
-                                                                  context:
-                                                                      context,
-                                                                  builder:
-                                                                      (alertDialogContext) {
-                                                                    return AlertDialog(
-                                                                      content: Text(
-                                                                          'Remove Donation?'),
-                                                                      actions: [
-                                                                        TextButton(
-                                                                          onPressed: () => Navigator.pop(
-                                                                              alertDialogContext,
-                                                                              false),
-                                                                          child:
-                                                                              Text('Cancel'),
-                                                                        ),
-                                                                        TextButton(
-                                                                          onPressed: () => Navigator.pop(
-                                                                              alertDialogContext,
-                                                                              true),
-                                                                          child:
-                                                                              Text('Confirm'),
-                                                                        ),
-                                                                      ],
-                                                                    );
-                                                                  },
-                                                                ) ??
-                                                                false;
-                                                      },
-                                                    ),
-                                                  ],
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              );
+                              return GetDonationsData(
+                                  donationsID: restaurantDonationIDs[index]);
                             },
                           );
                         }
