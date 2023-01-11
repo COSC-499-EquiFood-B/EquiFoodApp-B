5686cf8754bccd303e2f1f06e7d20d2d45f54b54
Anamica S <71396652+anamicasunder@users.noreply.github.com>
2023-01-11 10:37:55 -0800

    Update default-stats.dart

--- lib/indiv_dashboard/default-stats.dart
+++ lib/indiv_dashboard/default-stats.dart
@@ -1,22 +1,3 @@
-a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  a
-  
-
 import '../flutter_flow/flutter_flow_animations.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
