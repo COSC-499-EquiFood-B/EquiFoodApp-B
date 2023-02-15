24ec4648cdb76dc38fbff86d710ab16c92a2657d
Minh Bui <buithienminh@gmail.com>
2023-02-15 10:24:45 -0800

    Rename files and connect stat pages

--- lib/confirmation/confirmation.dart
+++ lib/confirmation/confirmation.dart
@@ -1,5 +1,5 @@
 import 'package:equi_food_app/index.dart';
-import 'package:equi_food_app/indiv_dashboard/indivdash1cont.dart';
+import 'package:equi_food_app/indiv_dashboard/indivDashboard.dart';
 
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_timer.dart';
