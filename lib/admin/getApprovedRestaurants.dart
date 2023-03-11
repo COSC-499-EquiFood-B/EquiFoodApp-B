96796d3b0bdae95b674308f8750b2846cf5fb456
minhhbt <buithienminh@gmail.com>
2023-03-10 21:23:53 -0800

    Test

--- lib/admin/getApprovedRestaurants.dart
+++ lib/admin/getApprovedRestaurants.dart
@@ -1,1 +1,135 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equi_food_app/admin/approvedrestaurants.dart';
+import 'package:equi_food_app/admin/approveordeny.dart';
+import 'package:equi_food_app/flutter_flow/flutter_flow_util.dart';
+import 'package:equi_food_app/index.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+import '../flutter_flow/flutter_flow_icon_button.dart';
+import '../flutter_flow/flutter_flow_theme.dart';
+
+class getRestaurants extends StatelessWidget {
+  final String restaurantIDs;
+
+  getRestaurants({required this.restaurantIDs});
+
+  @override
+  //create reference to the "donations" collection in firebase
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  Widget build(BuildContext context) {
+    return FutureBuilder<DocumentSnapshot>(
+        future: users.doc(restaurantIDs).get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> restaurantsData =
+                snapshot.data!.data() as Map<String, dynamic>;
+
+            return Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+              child: ListView(
+                addRepaintBoundaries: false,
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    child: Container(
+                      width: 100,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 0,
+                            color: Color(0xFFE0E3E7),
+                            offset: Offset(0, 1),
+                          )
+                        ],
+                      ),
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 44,
+                              height: 44,
+                              decoration: BoxDecoration(
+                                color: Color(0xFF4B39EF),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(2, 2, 2, 2),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(44),
+                                  child: Image.network(
+                                    restaurantsData["profile_img"],
+                                    width: 44,
+                                    height: 44,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding:
+                                    EdgeInsetsDirectional.fromSTEB(12, 0, 0, 0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          0, 0, 0, 4),
+                                      child: Text(
+                                        restaurantsData["restaurant_name"],
+                                        style: FlutterFlowTheme.of(context)
+                                            .title3
+                                            .override(
+                                              fontFamily: 'Inter',
+                                              color: Color(0xFF101213),
+                                              fontSize: 24,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                      ),
+                                    ),
+                                    Text(
+                                      restaurantsData["email"],
+                                      style: FlutterFlowTheme.of(context)
+                                          .bodyText2
+                                          .override(
+                                            fontFamily: 'Inter',
+                                            color: Color(0xFF57636C),
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.normal,
+                                          ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Icon(
+                              Icons.chevron_right_rounded,
+                              color: Color(0xFF57636C),
+                              size: 24,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+          return Center();
+        });
+  }
+}
