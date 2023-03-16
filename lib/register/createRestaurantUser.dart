c62489cb5a7f213ab2f3bcea5b39e838ae799669
minhhbt <buithienminh@gmail.com>
2023-03-15 18:20:37 -0700

    Add default thumbnail

--- lib/register/createRestaurantUser.dart
+++ lib/register/createRestaurantUser.dart
@@ -116,7 +116,9 @@ class _CreateRestaurantUserWidgetState
                     "is_approved":
                         false, // boolean field to signify that account needs approval from admin
                     "user_type":
-                        2 // user_type field (1 = Individual User, 2 = Restaurant User)
+                        2, // user_type field (1 = Individual User, 2 = Restaurant User)
+                    "profile_img":
+                        "https://images.unsplash.com/photo-1622115837997-90c89ae689f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
                   })
                 });
       } on FirebaseAuthException catch (e) {
