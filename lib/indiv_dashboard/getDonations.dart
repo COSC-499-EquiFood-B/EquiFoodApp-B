b5e5b5212c77fd5694b584ee005192d51665449f
Neilansh Rajpal <neilanshjr@gmail.com>
2023-03-27 20:05:44 -0700

    successfully replaced FutureBuilder with StreamBuilder on Indiv Dashboard; issue #389

--- lib/indiv_dashboard/getDonations.dart
+++ lib/indiv_dashboard/getDonations.dart
@@ -9,8 +9,13 @@ import '../flutter_flow/flutter_flow_theme.dart';
 
 class getDonations extends StatelessWidget {
   final String donationsID;
+  final Map<String, dynamic> donationData;
 
-  getDonations({required this.donationsID});
+  /* 2 things are required and sent to this Widget:
+      1. donation ID
+      2. Map<String, dynamic> containing its info (for eg. {item_name, item_img, description, etc.})
+  */
+  getDonations({required this.donationsID, required this.donationData});
 
   @override
   //create reference to the "donations" collection in firebase
@@ -18,101 +23,85 @@ class getDonations extends StatelessWidget {
       FirebaseFirestore.instance.collection('donations');
 
   Widget build(BuildContext context) {
-    return FutureBuilder<DocumentSnapshot>(
-        future: donations.doc(donationsID).get(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            Map<String, dynamic> donationsData =
-                snapshot.data!.data() as Map<String, dynamic>;
-
-            return Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(8, 0, 8, 0),
-              child: Wrap(
-                alignment: WrapAlignment.start,
-                crossAxisAlignment: WrapCrossAlignment.start,
-                direction: Axis.horizontal,
-                runAlignment: WrapAlignment.start,
-                verticalDirection: VerticalDirection.down,
-                clipBehavior: Clip.none,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                IndivItemWidget(donationsID: donationsID)),
-                      );
-                    },
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.95,
-                      height: 190,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 4,
-                              color: Color(0x230E151B),
-                              offset: Offset(0, 2),
-                            )
-                          ],
-                          borderRadius: BorderRadius.circular(12)),
-                      child: Padding(
-                        padding: EdgeInsetsDirectional.fromSTEB(8, 4, 4, 4),
-                        child: Column(
-                          mainAxisSize: MainAxisSize.max,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.network(
-                                '${donationsData["item_img"]}',
-                                width: double.infinity,
-                                height: 115,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  EdgeInsetsDirectional.fromSTEB(8, 12, 0, 0),
-                              child: Text(
-                                '${donationsData["restaurant_name"]}',
-                                style: FlutterFlowTheme.of(context)
-                                    .subtitle1
-                                    .override(
-                                      fontFamily: 'Inter',
-                                      color: Color(0xFF14181B),
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  EdgeInsetsDirectional.fromSTEB(8, 4, 0, 0),
-                              child: Text(
-                                'Meals Available: ${donationsData["quantity"]}',
-                                style: FlutterFlowTheme.of(context)
-                                    .bodyText2
-                                    .override(
-                                      fontFamily: 'Inter',
-                                      color: Color(0xFF57636C),
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                              ),
-                            ),
-                          ],
-                        ),
+    return Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(8, 0, 8, 0),
+      child: Wrap(
+        alignment: WrapAlignment.start,
+        crossAxisAlignment: WrapCrossAlignment.start,
+        direction: Axis.horizontal,
+        runAlignment: WrapAlignment.start,
+        verticalDirection: VerticalDirection.down,
+        clipBehavior: Clip.none,
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        IndivItemWidget(donationsID: donationsID)),
+              );
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.95,
+              height: 190,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 4,
+                      color: Color(0x230E151B),
+                      offset: Offset(0, 2),
+                    )
+                  ],
+                  borderRadius: BorderRadius.circular(12)),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(8, 4, 4, 4),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        '${donationData["item_img"]}',
+                        width: double.infinity,
+                        height: 115,
+                        fit: BoxFit.cover,
                       ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(8, 12, 0, 0),
+                      child: Text(
+                        '${donationData["restaurant_name"]}',
+                        style: FlutterFlowTheme.of(context).subtitle1.override(
+                              fontFamily: 'Inter',
+                              color: Color(0xFF14181B),
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(8, 4, 0, 0),
+                      child: Text(
+                        'Meals Available: ${donationData["quantity"]}',
+                        style: FlutterFlowTheme.of(context).bodyText2.override(
+                              fontFamily: 'Inter',
+                              color: Color(0xFF57636C),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                            ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            );
-          }
-          return Center();
-        });
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
